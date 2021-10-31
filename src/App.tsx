4e37836232bd10fcdfb38b6bc005a06d76357964
phuong.vo <phuong.vo@gensolve.com>
2021-10-31 13:45:33 +1300

    - Initiate ReactJs project with TypeScript and Redux

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div> Technical Assessment</div>
+    </div>
+  );
+}
+
+export default App;
