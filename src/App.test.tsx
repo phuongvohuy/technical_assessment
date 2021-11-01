47799ac7d1485bf5500c93c98dd0b4c128ad3560
phuong.vo <phuong.vo@gensolve.com>
2021-11-01 23:31:52 +1300

    Add Routing Testing(SignIn Routing, Home Routing, About Routing) for Application.

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,15 +1,73 @@
-import React from 'react';
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import { store } from './app/store';
 import App from './App';
+import {createMemoryHistory} from 'history'
+import { BrowserRouter, Router  } from "react-router-dom";
 
-test('renders learn react link', () => {
-  const { getByText } = render(
+test('Test Renders Application correctly === expect the root element has App class', () => {
+  const { container } = render(
     <Provider store={store}>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </Provider>
   );
 
-  expect(getByText(/learn/i)).toBeInTheDocument();
+  const appDivElement = container.querySelector(".App");
+  if (appDivElement) {
+    expect(appDivElement).toBeInTheDocument();
+  } else {
+    throw new Error('Can not find App class')
+  }
 });
+
+test('Test Routing === Render Signin page ', () => {
+  const history = createMemoryHistory()
+  history.push('/');
+  const { getByText } = render(
+    <Provider store={store}>
+      <Router history={history}>
+        <App />
+      </Router>
+    </Provider>
+  )
+
+  expect(getByText(/Sign In/i)).toBeInTheDocument();
+})
+
+test('Test Routing === Render Card Registration page ', () => {
+  const history = createMemoryHistory()
+  history.push('/home')
+  const { getByText } = render(
+    <Provider store={store}>
+      <Router history={history}>
+        <App />
+      </Router>
+    </Provider>
+  )
+  expect(getByText(/Welcome/i)).toBeInTheDocument();
+})
+
+test('Test Routing === Render About Page ', () => {
+  const history = createMemoryHistory()
+  history.push('/home')
+  const { getByText, container } = render(
+    <Provider store={store}>
+      <Router history={history}>
+        <App />
+      </Router>
+    </Provider>
+  )
+
+  const aboutMenuItem = container.querySelector(".about-menu-item");
+  if (aboutMenuItem) {
+    fireEvent.click(aboutMenuItem);
+    expect(getByText(/technical assessment/i)).toBeInTheDocument();
+  } else {
+    throw new Error('Can not find Menu About Item');
+  }
+})
+
+
+
